529440080a5b5501ec798d110a62e47c8cad2529
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-06-24 17:46:28 +0200

    Add memalign-free test

---
+++ test/memalign-free.c
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../include/slimguard.h"
+#include "../include/slimguard-large.h"
+#include "../include/sll.h"
+
+TEST_CASE("memalign", "[slimguard]")
+{
+    void *ptr = xxmemalign(128, 128);
+    REQUIRE(ptr);
+    REQUIRE(!((uint64_t)ptr % 128));
+    memset(ptr, 0x0, 128);
+    free(ptr);
+}
