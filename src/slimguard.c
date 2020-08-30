788ee7422bf8232528ef7793c903a84e4ff5f655
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2020-08-30 21:00:10 +0200

    faster log2

--- src/slimguard.c
+++ src/slimguard.c
@@ -14,17 +14,6 @@
 #include <err.h>
 #include <pthread.h>
 
-const int tab64[64] = {
-  63,  0, 58,  1, 59, 47, 53,  2,
-  60, 39, 48, 27, 54, 33, 42,  3,
-  61, 51, 37, 40, 49, 18, 28, 20,
-  55, 30, 34, 11, 43, 14, 22,  4,
-  62, 57, 46, 52, 38, 26, 32, 41,
-  50, 36, 17, 19, 29, 10, 13, 21,
-  56, 45, 25, 31, 35, 16,  9, 12,
-  44, 24, 15,  8, 23,  7,  6,  5
-};
-
 typedef struct cls_t{
   void* bucket[BKT];        // array for randomization and OP entropy
   void* start;              // start address of a size class
@@ -366,16 +355,9 @@ uint16_t find_sz_cls(void *ptr) {
     return 255;
 }
 
-/* a fast way to get the integer part of log2 */
-int log2_64 (uint64_t value) {
-  value |= value >> 1;
-  value |= value >> 2;
-  value |= value >> 4;
-  value |= value >> 8;
-  value |= value >> 16;
-  value |= value >> 32;
-
-  return tab64[((uint64_t)((value - (value >> 1))*0x07EDD5E59A4E28C2)) >> 58];
+int log2_64 (uint64_t u) {
+  if (u == 0) { return INT32_MIN; }
+  return ((int64_t)63 - (int64_t)__builtin_clzll(u));
 }
 
 /* SlimGuard malloc algorithm */
